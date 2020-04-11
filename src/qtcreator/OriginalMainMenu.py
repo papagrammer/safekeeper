c2d88d4e9ad774861360a7c89277c65928abca9f
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2020-04-11 14:18:27 +0300

    finish ui prototype code

--- src/qtcreator/OriginalMainMenu.py
+++ src/qtcreator/OriginalMainMenu.py
@@ -8,59 +8,9 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from .ActivityLogWindow import Ui_ActivityLogWindow
-from .DeleteUserWidget import Ui_DeleteUserWidget
-from .DroneWindow import Ui_DroneWindow
-from .InsertUserWidget import Ui_InsertUserWidget
-from .SilentAlarmWidget import Ui_SilentAlarmWidget
-from .IncidentReport import Ui_IncidentReportWindow
-from .NotificationWidget import Ui_NotificationWidget
 
-from .notif_util import sample_notification
 
 class Ui_MainMenuWindow(object):
-    def __init__(self, ctx):
-        self.ctx = ctx
-
-    def _activity_log_clicked(self):
-        # sample_notification(self.ctx)
-
-        # return
-        self.ActivityLogWindow = QtWidgets.QMainWindow()
-        self.ui_activity_log = Ui_ActivityLogWindow(self.ctx)
-        self.ui_activity_log.setupUi(self.ActivityLogWindow)
-        self.ActivityLogWindow.show()
-
-    def _delete_button_clicked(self):
-        self.DeleteUserWindow = QtWidgets.QMainWindow()
-        self.ui_delete_window = Ui_DeleteUserWidget(self.ctx)
-        self.ui_delete_window.setupUi(self.DeleteUserWindow)
-        self.DeleteUserWindow.show()
-
-    def _drone_button_clicked(self):
-        self.DroneControlWindow = QtWidgets.QMainWindow()
-        self.ui_drone_control = Ui_DroneWindow(self.ctx)
-        self.ui_drone_control.setupUi(self.DroneControlWindow)
-        self.DroneControlWindow.show()
-
-    def _incident_submission_clicked(self):
-        self.IncidentSubmissionWindow = QtWidgets.QMainWindow()
-        self.ui_report = Ui_IncidentReportWindow(self.ctx)
-        self.ui_report.setupUi(self.IncidentSubmissionWindow)
-        self.IncidentSubmissionWindow.show()
-
-    def _insert_button_clicked(self):
-        self.InsertButtonWindow = QtWidgets.QMainWindow()
-        self.ui_insert = Ui_InsertUserWidget(self.ctx)
-        self.ui_insert.setupUi(self.InsertButtonWindow)
-        self.InsertButtonWindow.show()
-
-    def _silent_alarm_clicked(self):
-        self.SilentAlarmWindow = QtWidgets.QMainWindow()
-        self.ui_alarm = Ui_SilentAlarmWidget(self.ctx)
-        self.ui_alarm.setupUi(self.SilentAlarmWindow)
-        self.SilentAlarmWindow.show()
-
     def setupUi(self, MainMenuWindow):
         MainMenuWindow.setObjectName("MainMenuWindow")
         MainMenuWindow.resize(424, 254)
@@ -87,7 +37,8 @@ class Ui_MainMenuWindow(object):
         self.listWidget = QtWidgets.QListWidget(self.centralwidget)
         self.listWidget.setGeometry(QtCore.QRect(160, 40, 256, 171))
         self.listWidget.setObjectName("listWidget")
-
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
         self.LatestIncidentsText = QtWidgets.QLabel(self.centralwidget)
         self.LatestIncidentsText.setGeometry(QtCore.QRect(160, 20, 131, 21))
         self.LatestIncidentsText.setObjectName("LatestIncidentsText")
@@ -96,24 +47,10 @@ class Ui_MainMenuWindow(object):
         self.IncidentNumberText.setObjectName("IncidentNumberText")
         MainMenuWindow.setCentralWidget(self.centralwidget)
 
-        ## Click Listeners
-        self.ActivityLogButton.clicked.connect(self._activity_log_clicked)
-        self.DeleteUserButton.clicked.connect(self._delete_button_clicked)
-        self.DroneControlButton.clicked.connect(self._drone_button_clicked)
-        self.IncidentSubmissionButton.clicked.connect(self._incident_submission_clicked)
-        self.InsertUserButton.clicked.connect(self._insert_button_clicked)
-        self.SilentAlarmButton.clicked.connect(self._silent_alarm_clicked)
-
-        ## how to attach notifications
-        item = QtWidgets.QListWidgetItem()
-        self.listWidget.addItem(item)
-        item.setText("Check In - Now - Front Desk")
-
         self.retranslateUi(MainMenuWindow)
         QtCore.QMetaObject.connectSlotsByName(MainMenuWindow)
 
     def retranslateUi(self, MainMenuWindow):
-        self.MainMenuWindow = MainMenuWindow
         _translate = QtCore.QCoreApplication.translate
         MainMenuWindow.setWindowTitle(_translate("MainMenuWindow", "Safeguard"))
         self.ActivityLogButton.setText(_translate("MainMenuWindow", "Activity Log"))
@@ -124,6 +61,18 @@ class Ui_MainMenuWindow(object):
         self.SilentAlarmButton.setText(_translate("MainMenuWindow", "Silent Alarm"))
         __sortingEnabled = self.listWidget.isSortingEnabled()
         self.listWidget.setSortingEnabled(False)
+        item = self.listWidget.item(0)
+        item.setText(_translate("MainMenuWindow", "ena"))
         self.listWidget.setSortingEnabled(__sortingEnabled)
         self.LatestIncidentsText.setText(_translate("MainMenuWindow", "Latest Incidents:"))
-        self.IncidentNumberText.setText(_translate("MainMenuWindow", "# Incident(s)"))
+        self.IncidentNumberText.setText(_translate("MainMenuWindow", "# Incidents"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainMenuWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainMenuWindow()
+    ui.setupUi(MainMenuWindow)
+    MainMenuWindow.show()
+    sys.exit(app.exec_())
--- src/ui/NotificationWidget.py
+++ src/ui/NotificationWidget.py
@@ -12,37 +12,52 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_NotificationWidget(object):
 
-    def __init__(self, ctx):
+    def __init__(self, ctx, add_buttons=False):
         self.ctx = ctx
+        self.add_buttons = add_buttons
 
     def setupUi(self, NotificationWidget):
-        NotificationWidget.setObjectName("NotificationWidget")
-        NotificationWidget.resize(276, 167)
-        self.ApproveButton = QtWidgets.QPushButton(NotificationWidget)
-        self.ApproveButton.setGeometry(QtCore.QRect(190, 140, 83, 25))
-        self.ApproveButton.setObjectName("ApproveButton")
-        self.AlarmButton = QtWidgets.QPushButton(NotificationWidget)
-        self.AlarmButton.setGeometry(QtCore.QRect(100, 140, 83, 25))
-        self.AlarmButton.setObjectName("AlarmButton")
-        self.NotificationText = QtWidgets.QLabel(NotificationWidget)
-        self.NotificationText.setGeometry(QtCore.QRect(10, 30, 261, 101))
-        self.NotificationText.setAlignment(QtCore.Qt.AlignCenter)
+        if self.add_buttons:
+            NotificationWidget.setObjectName("NotificationWidget")
+            NotificationWidget.resize(276, 167)
+
+        if self.add_buttons:
+            self.ApproveButton = QtWidgets.QPushButton(NotificationWidget)
+            self.ApproveButton.setGeometry(QtCore.QRect(190, 140, 83, 25))
+            self.ApproveButton.setObjectName("ApproveButton")
+            self.AlarmButton = QtWidgets.QPushButton(NotificationWidget)
+            self.AlarmButton.setGeometry(QtCore.QRect(100, 140, 83, 25))
+            self.AlarmButton.setObjectName("AlarmButton")
+
+        self.NotificationText = QtWidgets.QLabel()
+        
+        if self.add_buttons:
+            self.NotificationText.setGeometry(QtCore.QRect(10, 30, 261, 101))
+        self.NotificationText.setAlignment(QtCore.Qt.AlignLeft)
         self.NotificationText.setObjectName("NotificationText")
-        self.NotificationTimestamp = QtWidgets.QLabel(NotificationWidget)
-        self.NotificationTimestamp.setGeometry(QtCore.QRect(150, 0, 131, 21))
+        self.NotificationTimestamp = QtWidgets.QLabel()
+        if self.add_buttons:
+            self.NotificationTimestamp.setGeometry(QtCore.QRect(150, 0, 131, 21))
         self.NotificationTimestamp.setObjectName("NotificationTimestamp")
-        self.NotificationType = QtWidgets.QLabel(NotificationWidget)
-        self.NotificationType.setGeometry(QtCore.QRect(0, 0, 64, 21))
+        self.NotificationType = QtWidgets.QLabel()
+        self.NotificationType.setAlignment(QtCore.Qt.AlignCenter)
+        if self.add_buttons:
+            self.NotificationType.setGeometry(QtCore.QRect(0, 0, 64, 21))
         self.NotificationType.setObjectName("NotificationType")
 
         self.retranslateUi(NotificationWidget)
-        QtCore.QMetaObject.connectSlotsByName(NotificationWidget)
+        if self.add_buttons:
+            QtCore.QMetaObject.connectSlotsByName(NotificationWidget)
 
     def retranslateUi(self, NotificationWidget):
         _translate = QtCore.QCoreApplication.translate
-        NotificationWidget.setWindowTitle(_translate("NotificationWidget", "/!\\ Notification"))
-        self.ApproveButton.setText(_translate("NotificationWidget", "Approve"))
-        self.AlarmButton.setText(_translate("NotificationWidget", "Alarm"))
-        self.NotificationText.setText(_translate("NotificationWidget", "Notification Text"))
-        self.NotificationTimestamp.setText(_translate("NotificationWidget", "DD/MM/YYYY HH:MM"))
-        self.NotificationType.setText(_translate("NotificationWidget", "Type"))
+        if self.add_buttons:
+            NotificationWidget.setWindowTitle(_translate("NotificationWidget", "Incoming Notification"))
+
+        if self.add_buttons:
+            self.ApproveButton.setText(_translate("NotificationWidget", "Approve"))
+            self.AlarmButton.setText(_translate("NotificationWidget", "Alarm"))
+
+        self.NotificationText.setText(_translate("NotificationWidget", self.ctx['title']))
+        self.NotificationTimestamp.setText(_translate("NotificationWidget", self.ctx['timestamp']))
+        self.NotificationType.setText(_translate("NotificationWidget", self.ctx['type']))
